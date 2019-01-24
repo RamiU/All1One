3ac3bcbdd19005ce600687bc338e27e654124a03
Ramy <ramiubiera97@gmail.com>
2019-01-23 23:50:28 -0400

    v2

---
+++ app/src/main/java/com/rami/app/MainActivity.java
@@ -0,0 +1,53 @@
+package com.rami.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.Toast;
+
+import com.rami.all1one.Animaciones;
+
+public class MainActivity extends AppCompatActivity {
+
+View view;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+         view = findViewById(R.id.boton);
+
+      // Animaciones.ocultar(view, 2000, ValueAnimator.INFINITE)
+       // .start();
+        //Animaciones.rotar(view,3000,360f,3).start();
+
+        //Animaciones.trasladar(view,2,4000,400,400).start();
+
+
+       // view.animate().setDuration(3000).alpha(0).setStartDelay(2000)
+        //.alpha(1).rotation(180);
+
+
+
+
+
+
+       ObjectAnimator mover = ObjectAnimator.ofFloat(view,
+                "translationX", -500f, 0f);
+        mover.setDuration(2000);
+        ObjectAnimator fadeIn = ObjectAnimator.ofFloat(view, "alpha",
+                0f, 1f);
+        fadeIn.setDuration(2000);
+        AnimatorSet animatorSet = new AnimatorSet();
+
+        animatorSet.play(mover).with(fadeIn);
+        animatorSet.start();
+    }
+}
---
+++ library/src/main/java/com/rami/all1one/Animaciones.java
@@ -0,0 +1,109 @@
+package com.rami.all1one;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.animation.ValueAnimator;
+import android.util.Property;
+import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.CycleInterpolator;
+
+import androidx.annotation.IntDef;
+
+public class Animaciones {
+
+    private static PropertyValuesHolder valuesHolder(Property<View, Float> propiedad, float... valores){
+        return PropertyValuesHolder.ofFloat(propiedad,valores);
+
+    }
+    public static ObjectAnimator ocultar(View view, long duracion,int nRepeat){
+        ObjectAnimator fadeOut = ObjectAnimator.ofFloat(view, "alpha",
+                0f,1f);
+        fadeOut.setDuration(duracion);
+        fadeOut.setRepeatMode(ValueAnimator.REVERSE);
+        fadeOut.setRepeatCount(nRepeat);
+        //fadeOut.start();
+        return fadeOut;
+    }
+    public static ObjectAnimator trasladar(View view,int nRepeat,int duracion,float posX,float posY){
+
+        PropertyValuesHolder trasX = valuesHolder(View.TRANSLATION_X,0,posX);
+        PropertyValuesHolder trasY = valuesHolder(View.TRANSLATION_Y,0,posY);
+
+        ObjectAnimator trasladar = ObjectAnimator.ofPropertyValuesHolder(view, trasX,trasY);
+        trasladar.setDuration(duracion);
+        trasladar.setInterpolator(new AccelerateDecelerateInterpolator());
+        trasladar.setRepeatCount(nRepeat);
+        trasladar.setRepeatMode(ValueAnimator.REVERSE);
+       // trasladar.start();
+        return trasladar;
+    }
+
+
+    public static ObjectAnimator rotar(View view,int duracion,float rotacion,int nRepeticion){
+        ObjectAnimator rotationAnimator = ObjectAnimator.ofFloat(view,
+                "rotation", rotacion);
+        rotationAnimator.setDuration(duracion);
+        rotationAnimator.setRepeatMode(ValueAnimator.REVERSE);
+        rotationAnimator.setRepeatCount(nRepeticion);
+        return rotationAnimator;
+    }
+    public static void SimpleRotacion(View view,float rotacion,int duracion,boolean isGradual) {
+        view.animate().rotation(rotacion).setDuration(duracion).
+        setInterpolator(isGradual ? new AccelerateDecelerateInterpolator() : null)
+        .start();
+    }
+
+    public static void Simplealpha(View view) {
+        view.animate().alpha(0).start();
+    }
+    public static void SimpleScale(View view,int duracion,float escalaX,float escalaY) {
+
+        view.animate().scaleX(escalaX).setDuration(duracion).scaleY(escalaY)
+        .setListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                animation.start();
+            }
+        });
+    }
+
+    public static void Simpletranslate(View view,int posX,int posY,long duracion) {
+        view.animate().translationX(posX).translationY(posY)
+                .setDuration(duracion)
+                .start();
+
+    }
+
+    /**
+     *
+     * @param repetir true si desea que este set se repita indefinidamente
+     * @param animacion una lista de animacion
+     */
+    public static void animationSet(final boolean repetir, final Animator... animacion){
+
+        AnimatorSet animatorSet = new AnimatorSet();
+
+            AnimatorSet.Builder  builder = animatorSet.play(animacion[0]);
+            for (int i = 1; i < animacion.length; i++)
+                builder.before(animacion[i]);
+
+        animatorSet.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+
+                if (repetir)
+                    animation.start();
+            }
+        });
+        animatorSet.start();
+
+    }
+}
